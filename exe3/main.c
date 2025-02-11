8b6da7e6bcab45c53933ad06d7f82fb488874da0
Giulia <giuliar1@al.insper.edu.br>
2025-02-11 15:25:29 -0300

    ex 3 corrigido

--- exe3/main.c
+++ exe3/main.c
@@ -20,23 +20,41 @@ int main() {
   gpio_init(LED_PIN_G);
   gpio_set_dir(LED_PIN_R, GPIO_OUT);
   gpio_set_dir(LED_PIN_G, GPIO_OUT);
-
+  
+  int r = 0;
+  int g = 0;
 
   while (true) {
-    if (!gpio_get(BTN_PIN)) {
+    if (!gpio_get(BTN_PIN) && r%2 == 0) {
       printf("Botao 1\n");
       gpio_put(LED_PIN_R, 1);
+      r++;
       while (!gpio_get(BTN_PIN)) {
       };
     }
-    gpio_put(LED_PIN_R, 0);
-    if (!gpio_get(BTN_PIN2)) {
+    else if (!gpio_get(BTN_PIN) && r%2 != 0) {
+      printf("Botao 1\n");
+      gpio_put(LED_PIN_R, 0);
+      r++;
+      while (!gpio_get(BTN_PIN)) {
+      };
+    }
+    
+    if (!gpio_get(BTN_PIN2) && g%2 == 0) {
       printf("Botao 2\n");
       gpio_put(LED_PIN_G, 1);
+      g++;
       while (!gpio_get(BTN_PIN2)) {
       };
     }
-    gpio_put(LED_PIN_G, 0);
+    else if (!gpio_get(BTN_PIN2) && g%2 != 0) {
+      printf("Botao 1\n");
+      gpio_put(LED_PIN_G, 0);
+      g++;
+      while (!gpio_get(BTN_PIN2)) {
+      };
+    }
+    
   }
   
 }
